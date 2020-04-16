7c9d01316b7337fbe66feffabe601065c909922d
John <s1178740@gmail.com>
2020-04-16 08:58:52 +0100

    Added to ReadMe and modified script
    Worked on getting the moving parts of the plane working and added screenshots to readme

--- Assets/Imports/FighterInterceptor/Script/FighterSurfacesControl.cs
+++ Assets/Imports/FighterInterceptor/Script/FighterSurfacesControl.cs
@@ -4,87 +4,74 @@ using UnityEngine;
 
 public class FighterSurfacesControl : MonoBehaviour
 {
-    public Texture2D tex;
     public Transform rightElevator, leftElevator;
     public Transform rightFlap, leftFlap;
     public Transform rightRudder, leftRudder;
     public float MRx, MRy, MRz;
-    public float maxAngle, flapMaxAngle, rudderMaxAngle;
-    public float rightElevatorSetAngle, leftElevatorSetAngle;
-    public bool useMouse;
     public bool turnFlap;
-    private bool f;
+    public bool turnRudder;
     public float turnSpeed;
     private float angleX;
 
-	// Use this for initialization
-	void Start () {
+    private const float MAX_ELEVATOR_ANGLE = 25;
+    private const float MAX_FLAP_ANGLE = 45;
+    private const float MAX_RUDDER_ANGLE = 30;
+
+    void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	    if (useMouse)
-	    {
-	        MRx = Mathf.Clamp(MRx + Input.GetAxis("Mouse Y")*0.05f, -1f, 1f);
-	        MRz = Mathf.Clamp(MRz - Input.GetAxis("Mouse X")*0.05f, -1f, 1f);
-	        MRy = Mathf.Clamp(MRy + Input.GetAxis("Yaw")*0.05f, -1f, 1f);
-	    }
-	    else
-	    {
-            MRx = Mathf.Clamp(MRx + Input.GetAxis("Vertical") * 0.05f, -1f, 1f);
-            MRz = Mathf.Clamp(MRz - Input.GetAxis("Horizontal") * 0.05f, -1f, 1f);
-            MRy = Mathf.Clamp(MRy + Input.GetAxis("Yaw") * 0.05f, -1f, 1f);   
-	    }
-	    if (!turnFlap)
-        {
-            if (Input.GetKeyDown("f")) { turnFlap = true; f = false; }
-        }
-        else
-        {
-            if (turnFlap)
-            {
-                if (Input.GetKeyDown("f")) { turnFlap = false; f = true; }
-            }
-        }
-        rightElevator.transform.localRotation = Quaternion.Euler(new Vector3((MRz + MRx) * maxAngle, rightElevatorSetAngle, 0));
-        leftElevator.transform.localRotation = Quaternion.Euler(new Vector3((-MRz + MRx) * maxAngle,
-            leftElevatorSetAngle, 0));
-        TransAng(rightRudder, 16, MRy * rudderMaxAngle, 107);
-        TransAng(leftRudder, -16, MRy * rudderMaxAngle, 73);
-	    if (turnFlap)
-	    {
-            angleX = Mathf.Lerp(angleX, 1, turnSpeed * Time.deltaTime);
-            TransAng(rightFlap, 6, angleX * flapMaxAngle, 5);
-            TransAng(leftFlap, -6, angleX * flapMaxAngle, -5);
-	    }
-	    else
-	    {
-            angleX = Mathf.Lerp(angleX, 0, turnSpeed * Time.deltaTime);
-            TransAng(rightFlap, 6, angleX * flapMaxAngle, 5);
-            TransAng(leftFlap, -6, angleX * flapMaxAngle, -5);
-	    }
-	}
-    float c1, c2, c3, s1, s2, s3;
-    float w, x, y, z;
-    void TransAng(Transform tr, float heading, float attitude, float bank)
-    {
-        c1 = Mathf.Cos(heading * Mathf.Deg2Rad / 2);
-        s1 = Mathf.Sin(heading * Mathf.Deg2Rad / 2);
-        c2 = Mathf.Cos(attitude * Mathf.Deg2Rad / 2);
-        s2 = Mathf.Sin(attitude * Mathf.Deg2Rad / 2);
-        c3 = Mathf.Cos(bank * Mathf.Deg2Rad / 2);
-        s3 = Mathf.Sin(bank * Mathf.Deg2Rad / 2);
 
-        w = c1 * c2 * c3 - s1 * s2 * s3;
-        x = s2 * c1 * c3 + s1 * c2 * s3;
-        y = s1 * c2 * c3 + s2 * c1 * s3;
-        z = s3 * c1 * c2 - s1 * s2 * c3;
+        MRx = Mathf.Clamp(MRx + Input.GetAxis("Pitch") * 0.05f, -1f, 1f);
+        MRz = Mathf.Clamp(MRz - Input.GetAxis("Roll") * 0.05f, -1f, 1f);
+        MRy = Mathf.Clamp(MRy + Input.GetAxis("Yaw") * 0.05f, -1f, 1f);
+
+        if (Input.GetKeyDown("f")) {
+            Toggle(turnFlap);
+        }
+
+
+        Toggle(turnRudder);
+
+        rightElevator.transform.localRotation = Quaternion.Euler(new Vector3((MRz + MRx) * MAX_ELEVATOR_ANGLE, 0, 0));
+        leftElevator.transform.localRotation = Quaternion.Euler(new Vector3((-MRz + MRx) * MAX_ELEVATOR_ANGLE, 0, 0));
+
+        TurnRudder();
+        TurnFlap();
+    }
+
+    private void Toggle(bool toggle) {
+        if (!toggle) toggle = true; 
+        else toggle = false;
+    }
+
+    private void TurnRudder() {
+        int angleX = turnRudder ? 1 : 0;
+        TranslateAngle(rightRudder, 16, angleX * MRy * MAX_RUDDER_ANGLE, 107);
+        TranslateAngle(leftRudder, -16, angleX * MRy * MAX_RUDDER_ANGLE, 73);
+    }
+
+    private void TurnFlap() {
+        angleX = Mathf.Lerp(angleX, turnFlap ? 1 : 0, turnSpeed * Time.deltaTime);
+        TranslateAngle(rightFlap, 6, angleX * MAX_FLAP_ANGLE, 5);
+        TranslateAngle(leftFlap, -6, angleX * MAX_FLAP_ANGLE, -5);
+    }
+
+    void TranslateAngle(Transform tr, float heading, float attitude, float bank) {
+
+        float c1 = Mathf.Cos(heading * Mathf.Deg2Rad / 2);
+        float s1 = Mathf.Sin(heading * Mathf.Deg2Rad / 2);
+        float c2 = Mathf.Cos(attitude * Mathf.Deg2Rad / 2);
+        float s2 = Mathf.Sin(attitude * Mathf.Deg2Rad / 2);
+        float c3 = Mathf.Cos(bank * Mathf.Deg2Rad / 2);
+        float s3 = Mathf.Sin(bank * Mathf.Deg2Rad / 2);
+
+        float w = c1 * c2 * c3 - s1 * s2 * s3;
+        float x = s2 * c1 * c3 + s1 * c2 * s3;
+        float y = s1 * c2 * c3 + s2 * c1 * s3;
+        float z = s3 * c1 * c2 - s1 * s2 * c3;
 
         tr.transform.localRotation = new Quaternion(x, y, z, w);
-    }
-    void OnGUI()
-    {
-        GUI.DrawTexture(new Rect(Screen.width * 0.5f * (-MRz + 1f) - 8, Screen.height * 0.5f * (-MRx + 1f) - 8, 16f, 16f), tex);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UnityFlightpathSimulator
 Unity front end of SimpleFlightpathSimulator
+![Screenshot1](./Images/Screenshot1.JPG)
+![Screenshot2](./Images/Screenshot2.JPG)
